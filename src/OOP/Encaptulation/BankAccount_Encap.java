665fe2c37059cc35730d6dfa0a3e67c76dfed267
Susilkumar Udhayakumar <87655701+Robobuzz13@users.noreply.github.com>
2026-05-07 18:54:43 +0530

    moved encaptulation under OOP folder

--- src/OOP/Encaptulation/BankAccount_Encap.java
+++ src/OOP/Encaptulation/BankAccount_Encap.java
@@ -1,4 +1,4 @@
-package Encaptulation;
+package OOP.Encaptulation;
 
 public class BankAccount_Encap {
 
--- src/OOP/Encaptulation/Obj_BankAccount_Encap.java
+++ src/OOP/Encaptulation/Obj_BankAccount_Encap.java
@@ -1,4 +1,4 @@
-package Encaptulation;
+package OOP.Encaptulation;
 
 public class Obj_BankAccount_Encap {
 
